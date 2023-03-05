2e50c82c6662009bba4a196d425acc849c931364
alimh17 <amohamadi17@gamil.com>
2023-03-05 13:24:57 +0330

    refactor

--- frontend/src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>Hello word</div>;
-}
-
-export default App;
--- frontend/src/utils/ProfileCondition.ts
+++
@@ -1,7 +0,0 @@
-export const ProfileCondition = (profile: string, username: string) => {
-  if (profile === username) {
-    return true;
-  } else {
-    return false;
-  }
-};
